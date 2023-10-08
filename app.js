7c630cd1b353381e8699698a854da4aeb9d6cbb0
Santus Ik <cierisdesantus@gmail.com>
2023-10-08 16:37:40 +0100

    additional fixing

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-//app.use(morgan("tiny"))
+app.use(morgan("tiny"))
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET));
 
